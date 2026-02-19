dffacfba081d1724f23853c6cd51b05e35bc19b1
frankabras <frankabras@users.noreply.github.com>
2026-02-19 12:05:58 +0100

    refactor(SR04): Update sensor into base and pulse classes
    Split the original SensorSR04 into a BaseSR04 (common logic) and PulseSR04 (pulse-based implementation). Added configurable trigger duration (low_power).
    This adaptation allows for the implementation of a second class to utilize the sensor's Serial mode, depending on the resistor value soldered at the R19/R27 location on the sensor board.

--- sensor_sr04.py
+++ sensor_sr04.py
@@ -1,117 +1,23 @@
 from machine import Pin, time_pulse_us
 from utime import sleep, sleep_ms, sleep_us
 
-class SensorSR04:
+DEFAULT_OFFSET = 20.0 # default sensor blind zone in cm
+
+class BaseSR04:
     """
-    Class to handle HC-SR04 ultrasonic distance sensor.
-    
+    Base class for SR04 ultrasonic distance sensor.
+
     Args:
-        trig_pin (int): GPIO pin connected to the TRIG pin of the sensor.
-        echo_pin (int): GPIO pin connected to the ECHO pin of the sensor.
-        sensor_offset (float): Offset in centimeters to account for sensor blind zone.
-        timeout_us (int, optional): Timeout for echo pulse measurement in microseconds. Default is 30000us.
+        sensor_offset (float, optional): Offset in centimeters to account for sensor blind zone.
     
-    Attributes:
-        trig (Pin): Pin object for the TRIG pin.
-        echo (Pin): Pin object for the ECHO pin.
-        sensor_offset (float): Offset in centimeters to account for sensor blind zone.
-        timeout_us (int): Timeout for echo pulse measurement in microseconds.
-
     Methods:
-        read_once(temperature_c: float = 20.0) -> float:
-            Perform a single distance measurement and return the distance in centimeters.
         read(samples: int = 5, delay: int = 50, temperature_c: float = 20.0) -> float:
-            Perform multiple distance measurements and return the median distance in centimeters.
+            Read the distance from the sensor multiple times and return the median value.
     """
-    def __init__(self,
-                 trig_pin: int,
-                 echo_pin: int,
-                 sensor_offset: float,
-                 timeout_us: int = 30000) -> None:
-        self.trig = Pin(trig_pin, Pin.OUT)
-        self.echo = Pin(echo_pin, Pin.IN)
+    def __init__(self, 
+                 sensor_offset: float = DEFAULT_OFFSET) -> None:
         self.sensor_offset = sensor_offset
-        self.timeout_us = timeout_us
 
-        # cache for speed of sound based on temperature to avoid recalculating it every measurement
-        self._cached_speed : float | None = None
-        self._cached_temp_c : float | None = None
-
-    def _trigger(self) -> None:
-        """
-        Send a trigger pulse to the ultrasonic sensor.
-
-        :return: None
-        :rtype: None
-        """
-        # Send a 10-microsecond pulse to trigger the measurement
-        self.trig.value(0)
-        sleep_us(2)
-        self.trig.value(1)
-        sleep_us(10)
-        self.trig.value(0)
-     
-    def _measure_echo(self) -> int:
-        """
-        Measure the duration of the echo pulse.
-
-        :return: Duration of the echo pulse in microseconds with negative value indicating timeout or error
-        :rtype: int 
-        """
-        return time_pulse_us(self.echo, 1, self.timeout_us)
-    
-    def _calc_sound_speed(self,
-                          temperature_c: float) -> float:
-        """
-        Calculate the speed of sound in air based on temperature.
-
-        :param temperature_c: Temperature in degrees Celsius
-        :type temperature_c: float
-        :return: Speed of sound in cm/us
-        :rtype: float
-        """
-        if self._cached_temp_c == temperature_c and self._cached_speed is not None:
-            return self._cached_speed
-        
-        speed_m_s = 331.3 + (0.606 * temperature_c)     # speed of sound in m/s
-        speed_cm_us = speed_m_s / 10_000                # convert to cm/us
-        self._cached_speed = speed_cm_us
-        self._cached_temp_c = temperature_c
-        return speed_cm_us
-    
-    def _raw_to_distance(self,
-                         duration_us: int,
-                         sound_speed: float) -> float:
-        """
-        Convert the raw echo duration to distance in centimeters.
-
-        :param duration_us: Duration of the echo pulse in microseconds
-        :type duration_us: int
-        :param sound_speed: Speed of sound in cm/us
-        :type sound_speed: float
-        :return: Distance from the sensor to the object in centimeters
-        :rtype: float
-        """
-        if duration_us < 0:
-            return -1.0  # indicate timeout or error
-        
-        return ((duration_us * sound_speed) / 2.0) - self.sensor_offset
-
-    def read_once(self,
-                  temperature_c: float = 20.0) -> float:
-        """
-        Read the distance from the sensor once and convert it to centimeters.
-        
-        :param temperature_c: Temperature in degrees Celsius
-        :type temperature_c: float
-        :return: Distance from the sensor to the object in centimeters
-        :rtype: float
-        """
-        self._trigger()
-        duration = self._measure_echo()
-        speed_cm_us = self._calc_sound_speed(temperature_c)
-        return self._raw_to_distance(duration, speed_cm_us)
-    
     def read(self, 
              samples: int = 5,
              delay: int = 50,
@@ -120,13 +26,9 @@ class SensorSR04:
         Read the distance from the sensor multiple times and return the median value.
         
         :param samples: Number of samples to take
-        :type samples: int
         :param delay: Delay between samples in milliseconds
-        :type delay: int
         :param temperature_c: Temperature in degrees Celsius
-        :type temperature_c: float
         :return: Median distance from the sensor to the object in centimeters
-        :rtype: float
         """
         distances = []
         attempts = 0
@@ -144,20 +46,127 @@ class SensorSR04:
             raise RuntimeError("Sensor read timeout or invalid measurement")
 
         distances.sort()
-        if samples % 2:
+        num_distances = len(distances)
+        if num_distances % 2:
             # If odd number of samples
-            return round(distances[samples // 2], 2)
+            return round(distances[num_distances // 2], 2)
         else:
             # If even number of samples
-            mid = samples // 2
+            mid = num_distances // 2
             return round((distances[mid - 1] + distances[mid]) / 2, 2)
+        
+class PulseSR04(BaseSR04):
+    """
+    Class to handle SR04 ultrasonic distance sensor using pulse measurement.
+
+    NOTE: The operation mode (Pulse, Pulse low power, Serial) is determined by 
+    the resistor value soldered at the R19/R27 location on the sensor board.
+    Refer to the sensor documentation for configuration details 
+
+    Args:
+        trig_pin (int): GPIO pin connected to the TRIG pin of the sensor.
+        echo_pin (int): GPIO pin connected to the ECHO pin of the sensor.
+        sensor_offset (float, optional): Offset in centimeters to account for sensor blind zone.
+        timeout_us (int, optional): Timeout for echo pulse measurement in microseconds. 
+            Default is 30000us.
+        low_power (bool, optional): If True, use a longer trigger pulse for low power mode. 
+            Default is False.
+
+    Methods:
+        read_once(temperature_c: float = 20.0) -> float:
+            Perform a single distance measurement and return the distance in centimeters.
+    """
+    def __init__(self,
+                 trig_pin: int,
+                 echo_pin: int,
+                 low_power: bool = False,
+                 timeout_us: int = 30000,
+                 sensor_offset: float = DEFAULT_OFFSET) -> None:
+
+        super().__init__(sensor_offset)
+        self.trig = Pin(trig_pin, Pin.OUT)
+        self.echo = Pin(echo_pin, Pin.IN)
+        self.timeout_us = timeout_us
+
+        # For low power mode, we can use a longer trigger pulse to ensure the sensor wakes up properly,
+        self._trig_duration_us = 2000 if low_power else 10
+
+        # cache for speed of sound based on temperature to avoid recalculating it every measurement
+        self._cached_speed : float | None = None
+        self._cached_temp_c : float | None = None
+    
+    def _trigger(self) -> None:
+        """
+        Send a trigger pulse to the ultrasonic sensor.
+
+        :param duration_us: Duration of the trigger pulse in microseconds.
+        :return: None
+        """
+        self.trig.value(0)
+        sleep_us(2)
+        self.trig.value(1)
+        sleep_us(self._trig_duration_us)
+        self.trig.value(0)
+     
+    def _measure_echo(self) -> float:
+        """
+        Measure the duration of the echo pulse.
+
+        :return: Duration of the echo pulse in microseconds with negative value indicating timeout or error.
+        """
+        return time_pulse_us(self.echo, 1, self.timeout_us)
+    
+    def _calc_sound_speed(self,
+                          temperature_c: float) -> float:
+        """
+        Calculate the speed of sound in air based on temperature.
+
+        :param temperature_c: Temperature in degrees Celsius.
+        :return: Speed of sound in cm/us
+        """
+        if self._cached_temp_c == temperature_c and self._cached_speed is not None:
+            return self._cached_speed
+        
+        speed_m_s = 331.3 + (0.606 * temperature_c)     # speed of sound in m/s
+        speed_cm_us = speed_m_s / 10_000                # convert to cm/us
+        self._cached_speed = speed_cm_us
+        self._cached_temp_c = temperature_c
+        return speed_cm_us
+    
+    def _raw_to_distance(self,
+                         duration_us: float,
+                         sound_speed: float) -> float:
+        """
+        Convert the raw echo duration to distance in centimeters.
+
+        :param duration_us: Duration of the echo pulse in microseconds.
+        :param sound_speed: Speed of sound in cm/us.
+        :return: Distance from the sensor to the object in centimeters.
+        """
+        if duration_us < 0:
+            return -1.0  # indicate timeout or error
+        
+        return ((duration_us * sound_speed) / 2.0) - self.sensor_offset
+
+    def read_once(self,
+                  temperature_c: float = 20.0) -> float:
+        """
+        Read the distance from the sensor once and convert it to centimeters.
+        
+        :param temperature_c: Temperature in degrees Celsius.
+        :return: Distance from the sensor to the object in centimeters.
+        """
+        self._trigger()
+        duration = self._measure_echo()
+        speed_cm_us = self._calc_sound_speed(temperature_c)
+        return self._raw_to_distance(duration, speed_cm_us)            
 
 if __name__ == "__main__":
     from volume_calculator import HexagonalPrismTank
 
     offset = 20.0 # sensor blind zone in cm
 
-    LevelSensor = SensorSR04(3, 4, offset)
+    LevelSensor = PulseSR04(trig_pin=3, echo_pin=4, sensor_offset=offset)
     tank = HexagonalPrismTank(250, 45.5, 59.5, 53, 74)
 
     # Perform the measurement and record the fuel oil level
